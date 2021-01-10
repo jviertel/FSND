b03522ba41e9521c22b806ce241429cdd7fd32ed
jviertel <jvierte02@gmail.com>
2021-01-09 19:52:51 -0600

    Fix node_modules

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
     url: 'dev-yk2mgtma.us.auth0.com', // the auth0 domain prefix
-    audience: 'http://127.0.0.1:5000', // the audience set for the auth0 app
+    audience: 'coffee_shop', // the audience set for the auth0 app
     clientId: 'QPELrOFzkXzVcbFlRZ1DaWr427WfcdBp', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
