117f1f8f20f43f54335e0922eb7eac12bf63a6af
jviertel <jvierte02@gmail.com>
2021-01-13 13:52:34 -0600

    PEP8 style conform

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -9,18 +9,20 @@ AUTH0_DOMAIN = 'dev-yk2mgtma.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffee_shop'
 
-## AuthError Exception
+# AuthError Exception
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
 '''
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
 
-## Auth Header
+# Auth Header
 
 '''
 @TODO implement get_token_auth_header() method
@@ -30,6 +32,8 @@ class AuthError(Exception):
         it should raise an AuthError if the header is malformed
     return the token part of the header
 '''
+
+
 def get_token_auth_header():
     if 'Authorization' not in request.headers:
         raise AuthError('No header present', 401)
@@ -38,11 +42,12 @@ def get_token_auth_header():
 
     split_header = header.split(' ')
 
-    if (len(split_header) !=2) or (split_header[0].lower() != 'bearer'): 
+    if (len(split_header) != 2) or (split_header[0].lower() != 'bearer'):
         raise AuthError('Malformed header', 401)
 
     token = split_header[1]
     return token
+
 
 '''
 @TODO implement check_permissions(permission, payload) method
@@ -52,9 +57,12 @@ def get_token_auth_header():
 
     it should raise an AuthError if permissions are not included in the payload
         !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
+    it should raise an AuthError if the requested permission
+    string is not in the payload permissions array
     return true otherwise
 '''
+
+
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
         raise AuthError('Permissions not in payload', 400)
@@ -75,8 +83,11 @@ def check_permissions(permission, payload):
     it should validate the claims
     return the decoded payload
 
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+    !!NOTE urlopen has a common certificate error described here:
+    https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
+
+
 def verify_decode_jwt(token):
     key_url = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     keys = json.loads(key_url.read())
@@ -94,8 +105,8 @@ def verify_decode_jwt(token):
                 'use': key['use'],
                 'n': key['n'],
                 'e': key['e']
-            } 
-    
+            }
+
     if rsa_key:
         try:
             payload = jwt.decode(token, rsa_key, algorithms=ALGORITHMS, audience=API_AUDIENCE, issuer='https://' + AUTH0_DOMAIN + '/')
@@ -109,12 +120,8 @@ def verify_decode_jwt(token):
 
         except Exception:
             raise AuthError('Cannot parse token', 400)
-    
+
     raise AuthError('Cannot find key', 400)
-
-
-
-
 
 
 '''
@@ -124,9 +131,13 @@ def verify_decode_jwt(token):
 
     it should use the get_token_auth_header method to get the token
     it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
+    it should use the check_permissions method validate claims
+    and check the requested permission
+    return the decorator which passes the decoded payload to the
+    decorated method
 '''
+
+
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
