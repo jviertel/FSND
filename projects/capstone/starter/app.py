86cf443a0ba439483a7ed71ace23ae0551294305
jviertel <jvierte02@gmail.com>
2021-01-22 17:25:09 -0600

    after_request

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -15,5 +15,10 @@ def create_app(test_config=None):
   
 APP = create_app()
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
