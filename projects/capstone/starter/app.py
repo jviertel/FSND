34b4e72589bd31b092fa9b8cd02380a2840fc162
jviertel <jvierte02@gmail.com>
2021-01-22 16:16:20 -0600

    Update app.py

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -3,23 +3,20 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
+from models import Pedal, Manufacturer
 
 database_path = 'postgresql://postgres:88c67e0d53bef241b661e0e3a6cb0cd1@localhost:5432/trivia'
-
-db = SQLAlchemy()
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   app.config["SQLALCHEMY_DATABASE_URI"] = database_path
   app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-  db.app = app
-  db.init_app(app)
-  db.create_all()
   CORS(app)
   return app
-
+  
 APP = create_app()
+db = SQLAlchemy(APP)
 migrate = Migrate(APP, db)
 
 
