5c7d1655461792688fe257588009505ebcdd94a7
jviertel <jvierte02@gmail.com>
2021-01-22 16:33:30 -0600

    Create models for Manufacturer and Pedal

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,6 +1,32 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
+
+db = SQLAlchemy()
+
+'''
+Manufacturer
+'''
+class Manufacturer(db.Model):
+    __tablename__ = 'Manufacturer'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    fb_link = db.Column(db.String(500))
+    website_link = db.Column(db.String(500))
+
+'''
+Pedal
+'''
+class Pedal(db.Model):
+    __tablename__ = 'Pedal'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    pedal_type = db.Column(db.String(120))
+    new_price = db.Column(db.String)
+    used_price = db.Column(db.String)
+    image_link = db.Column(db.String(500))
+
 
 
