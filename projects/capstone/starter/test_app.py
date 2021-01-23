513cf47d1e849f1bb24f0b49fe20a4765332610c
jviertel <jvierte02@gmail.com>
2021-01-23 17:45:55 -0600

    Test setup

--- projects/capstone/starter/test_app.py
+++ projects/capstone/starter/test_app.py
@@ -1,1 +1,24 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from app import create_app
+from models import Pedal, Manufacturer, setup_db
+
+class TestPedalsAPI(unittest.TestCase):
+
+    def setUp(self):
+        '''Run before tests'''
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_path = 'postgresql://postgres:88c67e0d53bef241b661e0e3a6cb0cd1@localhost:5432/pedalsdb_test'
+        setup_db(self.app, self.database_path)
+
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+
+    def tearDown(self):
+        '''Run after tests'''
+        pass
