cab65816fde21699007eec49f47dfea3326ee6a8
jviertel <jvierte02@gmail.com>
2021-01-23 18:21:59 -0600

    Test get manufacturers success and failure

--- projects/capstone/starter/test_app.py
+++ projects/capstone/starter/test_app.py
@@ -22,3 +22,24 @@ class TestPedalsAPI(unittest.TestCase):
     def tearDown(self):
         '''Run after tests'''
         pass
+    
+    #Test successful get request to /manufacturers endpoint
+    def test_get_manufacturers(self):
+        res = self.client().get('/manufacturers')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['num_manufacturers'])
+        self.assertTrue(len(data['manufacturers']))
+        self.assertEqual(data['success'], True)
+    
+    def test_manufacturers_404_page_out_of_bounds(self):
+        res = self.client().get('/manufacturers?page=5000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource was not found')
+
+if __name__ == '__main__':
+    unittest.main()
